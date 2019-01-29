840e88c79eb324fd7a95fe85d1b57d896e489e1b
Bmord01 <32937039+Bmord01@users.noreply.github.com>
2019-01-28 22:45:03 -0600

    Change Size

--- Csci463System/Csci463System/Form1.Designer.cs
+++ Csci463System/Csci463System/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(706, 449);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Csci463 System";
