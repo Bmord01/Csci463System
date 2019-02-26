462ea1b527491312cd797a474514a9a57a051877
Bmord01 <32937039+Bmord01@users.noreply.github.com>
2019-02-25 21:00:45 -0600

    Add Models and Tests for models and interfaces

---
+++ Csci463System/Csci463SystemTests/UserTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Csci463System.Models;
+namespace Csci463SystemTests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestCreators()
+        {
+            User newUser =new User();
+        }
+    }
+}
